1c0eca9ace9d0d1b99b7c6616759261e7d05fa21
Abel Gancsos <agancsos@users.noreply.github.com>
2022-04-08 09:33:05 -0400

    Convert to JSON API

--- jenkins_remote/jenkins_remote.py
+++ jenkins_remote/jenkins_remote.py
@@ -5,7 +5,7 @@
 # Author      : Abel Gancsos                                                  #
 # Description : Practice with Jenkins API.                                    #
 ###############################################################################
-import os, re, sys, requests_html;
+import json, os, sys, requests_html;
 from requests.auth import HTTPBasicAuth;
 class JenkinsSession:
     base_endpoint=None;username=None;pam=None;session=None;credential=None;
@@ -20,18 +20,27 @@ class JenkinsSession:
         return str(rsp.content, "utf-8");
     def get_job_log(self, name, build=None):
         rsp = None;
-        if build == None: rsp = self.session.get("{0}/job/{1}/lastBuild/consoleText".format(self.base_endpoint, name), auth=self.credential, allow_redirects=True);
-        else: rsp = self.session.get("{0}/job/{1}/{2}/consoleText".format(self.base_endpoint, name, build), auth=self.credential, allow_redirects=True);
+        if build == None: rsp = self.session.get("{0}/job/{1}/lastBuild/consoleText".format(self.base_endpoint, name), auth=self.credential, allow_redirects=False);
+        else: rsp = self.session.get("{0}/job/{1}/{2}/consoleText".format(self.base_endpoint, name, build), auth=self.credential, allow_redirects=False);
         return str(rsp.content, "utf-8");
     def get_last_build(self, name):
-        rsp = self.session.get("{0}/job/{1}/lastBuild/".format(self.base_endpoint, name), auth=self.credential, allow_redirects=True);
-        m = re.findall("#[^\s]+", str(rsp.content, "utf-8"));
-        if len(m) > 0: return m[0].replace("#", "");
+        rsp = self.session.get("{0}/job/{1}/lastBuild/api/json".format(self.base_endpoint, name), auth=self.credential, allow_redirects=False);
+        return rsp.json();
     def delete_job_run(self, name, build=None):
         rsp = None;
-        if build == None: build = self.get_last_build(name);
-        rsp = self.session.post("{0}/job/{1}/{2}/doDelete".format(self.base_endpoint, name, build), auth=self.credential, allow_redirects=True);
+        if build == None: build = self.get_last_build(name)["id"];
+        rsp = self.session.post("{0}/job/{1}/{2}/doDelete".format(self.base_endpoint, name, build), auth=self.credential, allow_redirects=False);
         return rsp.status_code == 200;
+    def list_jobs(self, path=None, recursive=False):
+        result = [];
+        rsp = self.session.get("{0}/api/json".format(self.base_endpoint), auth=self.credential, allow_redirects=False);
+        for job in rsp.json()["jobs"]:
+            if ".folder." in job["_class"]:
+                if recursive:
+                    temp = self.list_jobs(job["url"], recursive);
+                    for j in temp: result.append(j);
+            else: result.append(job);
+        return result;
     pass;
 
 if __name__ == "__main__":
@@ -45,3 +54,5 @@ if __name__ == "__main__":
     if operation == "trigger" : print(session.trigger_job("Test1"));
     elif operation == "log"   : print(session.get_job_log("Test1", build_num));
     elif operation == "delete": print(session.delete_job_run("Test1", build_num));
+    elif operation == "list"  : print(session.list_jobs());
+        
